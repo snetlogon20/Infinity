b6e916a419c4a460d5f94ae7011aae5b3aaee16e
snetlogon20 <snetlogon20@sina.com>
2025-03-30 14:58:11 +0800

    repo sync

--- CICD/git/gitsync.sh
+++ CICD/git/gitsync.sh
@@ -7,16 +7,22 @@ mkdir -p /D/workspace_python/giteeRepo/
 cd /D/workspace_python/giteeRepo/
 rm -rf /D/workspace_python/giteeRepo/infinity/
 git clone https://gitee.com/snetlogon20/infinity
-git pull origin main
+
+#cd /D/workspace_python/giteeRepo/infinity/Infinity/
+#git pull origin main
 
 mkdir -p /D/workspace_python/githubRepo/Infinity/
 cd /D/workspace_python/githubRepo/Infinity/
-rm -r /D/workspace_python/githubRepo/Infinity/dataIntegrator
-cp -r /D/workspace_python/giteeRepo/infinity/dataIntegrator /D/workspace_python/githubRepo/Infinity/dataIntegrator
-cd /D/workspace_python/githubRepo/Infinity/dataIntegrator
+rm -rf /D/workspace_python/githubRepo/Infinity/CICD
+rm -rf /D/workspace_python/githubRepo/Infinity/dataIntegrator
+cp -r /D/workspace_python/giteeRepo/infinity/CICD /D/workspace_python/githubRepo/Infinity
+cp -r /D/workspace_python/giteeRepo/infinity/dataIntegrator /D/workspace_python/githubRepo/Infinity
+
+cd /D/workspace_python/githubRepo/Infinity/
 
 git status
 git add .
 git status
 git commit -m "repo sync"
+git remote set-url origin https://github.com/snetlogon20/Infinity.git
 git push -u origin main
--- dataIntegrator/__init__.py
+++ dataIntegrator/__init__.py
@@ -1,6 +1,6 @@
 import os
 from dataIntegrator.TuShareService.TuShareServiceManager import TuShareServiceManager
-#20250401
+
 def getEnv():
     print("PYTHONPATH:", os.environ.get('PYTHONPATH'))
     print("PATH:", os.environ.get('PATH'))
